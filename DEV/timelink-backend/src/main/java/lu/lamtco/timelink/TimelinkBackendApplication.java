0dd2ff0f74ea8f7ae73edd8c56178f155be3aab9
marques-tomas-unilu <tomas.marques.001@student.uni.lu>
2025-09-30 13:56:02 +0200

    feat: added admin panel and backend project folder

---
+++ DEV/timelink-backend/src/main/java/lu/lamtco/timelink/TimelinkBackendApplication.java
@@ -0,0 +1,13 @@
+package lu.lamtco.timelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelinkBackendApplication.class, args);
+	}
+
+}
---
+++ DEV/timelink-backend/src/test/java/lu/lamtco/timelink/TimelinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lu.lamtco.timelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelinkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
